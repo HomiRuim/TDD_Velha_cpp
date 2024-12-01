c3eeabb88d08fc13ff0d5a593f73ef1029f9f583
DaviB <dvbueno7@gmail.com>
2024-12-01 00:54:06 -0300

    arquivos iniciais

---
+++ testa_velha.cpp
@@ -0,0 +1,21 @@
+/**
+ * @file TDD_velha/velha.cpp
+ * @brief verifica situacao do jogo da velha
+ * @author Programador
+ * * Copyright 2024 Davi Bueno
+ * @param  velha descreve o parametro
+ *
+ *  Descrever o que a funcao faz
+ */
+
+#include "velha.hpp"
+#define CATCH_CONFIG_MAIN
+#include "catch.hpp"
+
+TEST_CASE( "Testa velha", "[single-file]" ) {
+	int teste1[3][3]= {   { 2, 0, 1 },
+	                      { 2, 0, 1 },
+						  { 0, 2, 1 }
+					  };
+    REQUIRE( VerificaVelha(teste1) == 1 );
+}
---
+++ velha.cpp
@@ -0,0 +1,16 @@
+/**
+ * @file TDD_velha/velha.cpp
+ * @brief verifica situacao do jogo da velha
+ * @author Programador
+ * * Copyright 2024 Davi Bueno
+ * @param  velha descreve o parametro
+ *
+ *  Descrever o que a funcao faz
+ */
+
+#include "velha.hpp"
+
+int VerificaVelha( int velha[3][3] )
+{
+	return 0; /*!< retorna zero para teste */
+}
